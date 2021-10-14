9dd0e1c9e04760b78337dcb9ccafa32e6a56d9ff
DayDream-21 <vova13158@gmail.com>
2021-10-14 14:07:40 +0300

    Initial commit

---
+++ src/test/java/com/slavamashkov/spring/springboot/springboot_rest/SpringBootRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.spring.springboot.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
