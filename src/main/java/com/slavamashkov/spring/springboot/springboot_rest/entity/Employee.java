847c8337439531b190cd8351af4d84fa43874875
DayDream-21 <vova13158@gmail.com>
2021-10-14 15:04:36 +0300

    Copied logic from previous project Changed port from 8080 to 8081 in application.properties

---
+++ src/main/java/com/slavamashkov/spring/springboot/springboot_rest/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.slavamashkov.spring.springboot.springboot_rest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+}
